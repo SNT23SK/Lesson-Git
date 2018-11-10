9ad01aee1f9c1bb185f180b2b486a77c6eb80bf3
snt23skv <snt23skv@hotmail.com>
2018-11-10 14:30:33 +0400

    added type Car

---
+++ Lesson Git/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Git
+{
+    class Car
+    {
+        #region Variables
+        Double mileage;
+
+        bool IsDirty;
+        bool IsWorking;
+        #endregion
+        public Car()
+        {
+            mileage = 0;
+            IsDirty = false;
+            IsWorking = true;
+        }
+
+        #region Properties
+        public bool look
+        {
+            get { return IsDirty; }
+            set { IsDirty = value; }
+        }
+        public bool StateEngine
+        {
+            get { return IsWorking; }
+            set { IsWorking = value; }
+
+        }
+        public Double Mileage
+        {
+            get { return mileage; }
+           private set { }
+        }
+
+        
+
+
+
+
+    
+
+        #endregion
+
+        #region Methods
+
+        public void Travel(double path)
+        {
+
+        }
+        
+        #endregion
+    }
+}
--- Lesson Git/Program.cs
+++ Lesson Git/Program.cs
@@ -10,6 +10,8 @@ namespace Lesson_Git
     {
         static void Main(string[] args)
         {
+
         }
+
     }
 }
