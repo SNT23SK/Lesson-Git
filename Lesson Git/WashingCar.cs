8fba00f575c30f06f00852d3d53db58524ed90f4
Александр Нейчев <alexnejchev73@gmail.com>
2018-11-17 13:12:56 +0400

    added WashingCar

---
+++ Lesson Git/WashingCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Git
+{
+    /// <summary>
+    /// Статический класс Мойка
+    /// </summary>
+    public static class WashingCar
+    {
+    }
+}
