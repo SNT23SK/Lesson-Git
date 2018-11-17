e811d5910ba3dd4a50176e550ea713490628f390
Александр Нейчев <alexnejchev73@gmail.com>
2018-11-17 14:47:24 +0400

    added functions Washing and GetBill into WashingCar

--- Lesson Git/WashingCar.cs
+++ Lesson Git/WashingCar.cs
@@ -11,5 +11,22 @@ namespace Lesson_Git
     /// </summary>
     public static class WashingCar
     {
+        /// <summary>
+        /// Метод моет машину
+        /// </summary>
+        /// <param name="car">Эземпляр машинки</param>
+        public static void Washing(Car car)
+        {
+            car.look = false;
+        }
+
+        /// <summary>
+        /// Метод возвращает стоимость мойки машины
+        /// </summary>
+        /// <returns></returns>
+        public static int GetBill()
+        {
+            return 25;
+        }
     }
 }
